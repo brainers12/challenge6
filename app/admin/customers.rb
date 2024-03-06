044ec484586bff0f143f78563f69f615e7b34a8b
Navjot <navjotcheema289@gmail.com>
2024-03-05 19:00:49 -0600

    Initial commit - Setup Rails project with Customer model and Active Admin

---
+++ app/admin/customers.rb
@@ -0,0 +1,48 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :full_name, :phone_number, :email_address, :notes
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:full_name, :phone_number, :email_address, :notes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag customer.image, size: '150x150'
+        else
+          "No image attached"
+        end
+      end
+    end
+  end
+  
+end
